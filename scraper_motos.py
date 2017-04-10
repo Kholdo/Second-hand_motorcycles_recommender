42e3d8f16ef5a3c78f4a03d54bc7d6403a5821e9
Kholdo <koldopinaortiz@gmail.com>
2017-04-10 21:49:19 +0200

    beautifulsoup object and number of ads

--- scraper_motos.py
+++ scraper_motos.py
@@ -1,9 +1,18 @@
 #Scraper motos.net
 
 #libraries
-import pandas as pandas
+import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 import re
 import unicodedata
 
+#BeautifulSoup object
+motor_url = 'http://motos.coches.net/ocasion/?pg=2&Tops=1&or=-1&fi=SortDate'
+motos_req = requests.get(motor_url)
+motos_soup = BeautifulSoup(motos_req.text, "html.parser")
+
+ads_number_text = motos_soup.find_all("h1", {"class": "floatleft"})
+num_ads = int(re.findall(r'[^ ]*\.[^ ]*', ads_number_text[0].contents[0])[0].replace('.',''))
+
+print num_ads
