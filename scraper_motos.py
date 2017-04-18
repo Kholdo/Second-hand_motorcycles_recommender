9674736b7c7fa616b2b33a1084290770cf2b16f1
Kholdo <koldopinaortiz@gmail.com>
2017-04-18 22:39:40 +0200

    prints commented

--- scraper_motos.py
+++ scraper_motos.py
@@ -45,7 +45,7 @@ def scraper_motos():
 						bike_price = int(''.join(re.findall(r'\b\d+\b', link_soup.find(class_='pvp').contents[0])))
 					except:
 						bike_price = ''
-						print "price error in http://motos.coches.net" + link['href']
+						#print "price error in http://motos.coches.net" + link['href']
 					if len(title) == 4 and bike_price != '':						
 						#city, brand, model
 						bike_city, bike_brand, bike_model = ra(title[1].get_text()), ra(title[2].get_text()),ra(title[3].get_text())
@@ -55,33 +55,33 @@ def scraper_motos():
 							bike_type = link_soup.find(id = 'litTipo').contents[1].strip()
 						except:
 							bike_type = ''
-							print "type error in http://motos.coches.net" + link['href']
+							#print "type error in http://motos.coches.net" + link['href']
 						#cc
 						try:
 							bike_cc = int(''.join(re.findall(r'\b\d+\b', link_soup.find(id='litCC').contents[1].strip())))
 						except:
 							bike_cc = ''
-							print "cc error in http://motos.coches.net" + link['href']
+							#print "cc error in http://motos.coches.net" + link['href']
 						#color
 						try:
 							bike_color = re.findall(r'<b>Color:</b>([^<]*)', link_req.text)[0].strip()
 							bike_color = '' if bike_color == '-' else bike_color
 						except:
 							bike_color = ''
-							print "color error in http://motos.coches.net" + link['href']
+							#print "color error in http://motos.coches.net" + link['href']
 						#km
 						try: 
 							bike_km = re.findall(r'<b>Km:</b>([^<]*)', link_req.text)[0].strip().replace('.', '')
 							bike_km = '' if bike_km == '-' else bike_km
 						except:
 							bike_km = ''
-							print "km error in http://motos.coches.net" + link['href']
+							#print "km error in http://motos.coches.net" + link['href']
 						#year
 						try:
 							bike_year = re.findall(r'Año:</b>([^<]*)', link_req.text.encode('utf8'))[0].strip().replace('.','')
 						except:
 							bike_year = ''
-							print "year error in http://motos.coches.net" + link['href']
+							#print "year error in http://motos.coches.net" + link['href']
 						
 						matrioska_tb.append([bike_city, bike_brand, bike_model, bike_type, bike_cc, bike_color, bike_km, bike_year, bike_price])
 
