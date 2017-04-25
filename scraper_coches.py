204d44269117236c274d7f19f9a6f05a09f2c87c
Kholdo <koldopinaortiz@gmail.com>
2017-04-25 21:34:28 +0200

    scraping brand

--- scraper_coches.py
+++ scraper_coches.py
@@ -16,4 +16,26 @@ def scraper_coches():
 	pages_number_text = coches_soup.find_all("h1", {"class": "mt-SerpHeader-title u-ellipsis"})
 	num_pages = int(re.findall(r'[.0-9]*\)', pages_number_text[0].contents[0])[0].replace('.','')[:-1])
 
-	return num_pages
+	#Lets go to scrape the urls
+	matrioska_tb = []
+	matrioska_header = ['province', 'brand', 'model']
+
+	print 'Start time: %ss' % datetime.now()
+	print 'num_pages %d' %num_pages
+
+	for i in range(1, 2):
+		sub_url = 'http://www.coches.net/segunda-mano/?pg=%d' %i
+		sub_req = requests.get(sub_url, allow_redirects = False)
+		if sub_req.status_code == 200:
+			sub_soup = BeautifulSoup(sub_req.text, "html.parser")
+			links_list = sub_soup.find_all("a", {"class": "mt-CardAd-link"}, href = True)
+			for link in links_list:
+				link_req = requests.get("http://www.coches.net" + link['href'])
+				link_soup = BeautifulSoup(link_req.text, "html.parser")
+				# Brand
+				car_brand = link_soup.find_all("a", {"data-tagging": "c_detail_bread_ad_brand"})[0].contents[0]
+
+				matrioska_tb.append([car_brand])
+
+	print 'End time: %s' % datetime.now()
+	return matrioska_tb
