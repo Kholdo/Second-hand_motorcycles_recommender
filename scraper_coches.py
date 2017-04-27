f5dfce36b813d50ed733667fa5f12a41db124168
Kholdo <koldopinaortiz@gmail.com>
2017-04-27 20:23:04 +0200

    scraping gear

--- scraper_coches.py
+++ scraper_coches.py
@@ -59,13 +59,17 @@ def scraper_coches():
 					# hp
 					if len(re.findall('[ ]cv', item.get_text())) > 0:
 						car_hp = int(re.findall(r'[0-9]*', item.get_text().strip())[0].replace('.', ''))
-					# g
+					# gear
+					if len(re.findall('[ ]Cambio', item.get_text())) > 0:
+						car_gear = int(re.findall(r'[0-9]*', item.get_text().strip())[0].replace('.', ''))
+
 
 				matrioska_tb.append([car_brand, car_model, 
 									car_province, car_price, 
 									car_year, car_km, 
 									car_doors, car_seats,
-									car_hp])
+									car_hp, car_gear,
+									])
 
 	print 'End time: %s' % datetime.now()
 	return matrioska_tb
