ccba369e3028ada9853a9d82d667c0231cf649ef
Kholdo <koldopinaortiz@gmail.com>
2017-05-03 19:11:39 +0200

    scraper_coches and tools update

--- tools.py
+++ tools.py
@@ -1,3 +1,3 @@
 def remove_accents(s):
 	import unicodedata
-	return ''.join((c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn'))
+	return ''.join((c for c in unicodedata.normalize('NFD', unicode(s)) if unicodedata.category(c) != 'Mn'))
