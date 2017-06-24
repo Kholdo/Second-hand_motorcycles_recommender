7e29b6a2f81084242f8a17901c19e162b52531a9
Koldo Pina <koldopinaortiz@gmail.com>
2017-06-24 13:10:18 +0200

    variables none value condition

--- app/recomender.py
+++ app/recomender.py
@@ -14,20 +14,18 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def message():
+	title= "Motorcycle Recommender"
+	subtitle = "The best and simplest one in the whole world wide web"
 	query_city = request.form.get("cities")
 	query_brand = request.form.get("brands")
 	query_type = request.form.get("types")
 	query_year = request.form.get("years")
 	
-	#r = Recommender(query_city, query_brand, int(query_year), 2017, 20)
-	r = Recommender('madrid', 'bmw', 'custom', 2010, 20)
-
-	title= "Motorcycle Recommender"
-	subtitle = "The best and simplest one in the whole world wide web"
-
-	res = r.recommender().values.tolist()
-	if res == []:
-		res = ['noData']
+	if query_city != None and query_brand != None and query_type != None and query_year != None:
+		r = Recommender(query_city, query_brand, query_type, query_year, 20)
+		res = r.recommender().values.tolist()
+	else:
+		res = [[]]
 
 	return render_template("main.html", res = res, title = title, subtitle=subtitle, 
 							brands_list = brands_list, types_list=types_list, 
